d3ec81bf0573637c59506e874492db5461cb8561
Chanchana Sornsoontorn <off9955555@gmail.com>
2016-11-08 02:58:59 +0700

    Move player using WASD

--- OpenGLShootingGame/Main.cpp
+++ OpenGLShootingGame/Main.cpp
@@ -14,17 +14,25 @@ struct Point {
 };
 
 time_point<system_clock> START_TIME;
+time_point<system_clock> PREVIOUS_TIME;
 time_point<system_clock> CURRENT_TIME;
 duration<double> TIME; // time duration since the program is loaded
+duration<double> TIME_DELTA;
 int W = 800;
 int H = 600;
-vector<Point> points;
+float PLAYER_SPEED = 500.0f;
 
+vector<Point> points;
 Point playerPosition;
+Point playerSpeed;
 float playerAngle = 0;
 
 double time() {
 	return TIME.count(); // returns time since game loaded in seconds
+}
+
+double timeDelta() {
+	return TIME_DELTA.count();
 }
 
 void setDefaultColor() {
@@ -56,6 +64,7 @@ void drawRect(int glPrimitve, float w, float h) {
 
 void drawPlayer(float rad, Point gunSize) {
 	glPushMatrix();
+	glTranslatef(playerPosition.x, playerPosition.y, 0);
 	drawCircle(GL_POLYGON, { rad, rad });
 	glRotatef(playerAngle - 90, 0, 0, 1);
 	drawRect(GL_LINE_LOOP, gunSize.x, gunSize.y);
@@ -98,12 +107,16 @@ void click(int btn, int st, int x, int y) {
 void update() {
 	static int framesDrawn = 0;
 	static int lastTime = 0;
+	PREVIOUS_TIME = CURRENT_TIME;
 	CURRENT_TIME = system_clock::now();
 	TIME = CURRENT_TIME - START_TIME;
+	TIME_DELTA = CURRENT_TIME - PREVIOUS_TIME;
 	if ((int)time() > lastTime) {
 		lastTime = (int)time();
 		cout << "Average FPS: " << (float) framesDrawn / lastTime << endl;
 	}
+	playerPosition.x += playerSpeed.x * timeDelta();
+	playerPosition.y += playerSpeed.y * timeDelta();
 	glutPostRedisplay();
 	framesDrawn++;
 }
@@ -113,9 +126,50 @@ void reshape(int w, int h) {
 	glutReshapeWindow(W, H);
 }
 
-void passiveMotion(int x, int y) {
+void pointTowards(int x, int y) {
 	Point p = screenToWorld(x, y);
-	playerAngle = atan2f(p.y, p.x) * 180 / PI; // make player gun points towards the mouse cursor
+	// make player gun points towards the mouse cursor
+	playerAngle = atan2f(p.y - playerPosition.y, p.x - playerPosition.x) * 180 / PI;
+}
+
+void passiveMotion(int x, int y) {
+	pointTowards(x, y);
+}
+
+void keyboard(unsigned char c, int x, int y) {
+	if (c == 'a') {
+		playerSpeed.x = -PLAYER_SPEED;
+	}
+	else if (c == 'd') {
+		playerSpeed.x = PLAYER_SPEED;
+
+	}
+	else if (c == 'w') {
+		playerSpeed.y = PLAYER_SPEED;
+
+	}
+	else if (c == 's') {
+		playerSpeed.y = -PLAYER_SPEED;
+	}
+	pointTowards(x, y);
+}
+
+void keyboardUp(unsigned char c, int x, int y) {
+	if (c == 'a') {
+		playerSpeed.x += PLAYER_SPEED;
+	}
+	else if (c == 'd') {
+		playerSpeed.x -= PLAYER_SPEED;
+
+	}
+	else if (c == 'w') {
+		playerSpeed.y -= PLAYER_SPEED;
+
+	}
+	else if (c == 's') {
+		playerSpeed.y += PLAYER_SPEED;
+	}
+	pointTowards(x, y);
 }
 
 void initialize() {
@@ -124,8 +178,11 @@ void initialize() {
 	glutMouseFunc(click);
 	glutReshapeFunc(reshape);
 	glutPassiveMotionFunc(passiveMotion);
+	glutKeyboardFunc(keyboard);
+	glutKeyboardUpFunc(keyboardUp);
 	points.push_back({ 0, 0 });
 	START_TIME = system_clock::now();
+	CURRENT_TIME = system_clock::now();
 }
 
 int main(int argc, char **argv) {

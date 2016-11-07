79a8abfdcdd17a0678b6b12f29dd9bf5902626c9
Chanchana Sornsoontorn <off9955555@gmail.com>
2016-11-08 01:16:34 +0700

    Add time mechanism

--- OpenGLShootingGame/Main.cpp
+++ OpenGLShootingGame/Main.cpp
@@ -1,27 +1,82 @@
 #include <iostream>
+#include <vector>
+#include <chrono>
 #include <gl/glut.h>
 using namespace std;
+using namespace chrono;
 
+struct Point {
+	float x;
+	float y;
+};
+
+time_point<system_clock> START_TIME;
+time_point<system_clock> CURRENT_TIME;
+duration<double> TIME; // time duration since the program is loaded
 int W = 500;
 int H = 500;
+vector<Point> points;
 
 void display() {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	gluOrtho2D(0, W, 0, H);
+	gluOrtho2D(-W/2, W/2, -H/2, H/2);
+
+	glColor3f(0, 0, 0);
+	glPointSize(10);
+	glBegin(GL_POINTS);
+	for (size_t i = 0; i < points.size(); i++)
+	{
+		glVertex2f(points[i].x, points[i].y);
+	}
+	glEnd();
 
 	glutSwapBuffers();
+}
+
+void click(int btn, int st, int x, int y) {
+	if (st == GLUT_DOWN) {
+		float sx = x - W / 2;
+		float sy = (H - y) - H / 2;
+		cout << sx << " " << sy << endl;
+		points.push_back({ sx, sy });
+	}
+}
+
+void update() {
+	static int cnt = 0;
+	CURRENT_TIME = system_clock::now();
+	TIME = CURRENT_TIME - START_TIME;
+	
+	if (++cnt % 1000 == 0) {
+		cout << TIME.count() << endl;
+	}
+	glutPostRedisplay();
+}
+
+void reshape(int w, int h) {
+	cout << "Please don't reshape my fragile window!" << endl;
+	glutReshapeWindow(W, H);
+}
+
+void initialize() {
+	glutDisplayFunc(display);
+	glutIdleFunc(update);
+	glutMouseFunc(click);
+	glutReshapeFunc(reshape);
+	points.push_back({ 0, 0 });
+	START_TIME = system_clock::now();
 }
 
 int main(int argc, char **argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 	glutInitWindowSize(W, H);
+	glutInitWindowPosition(700, 100);
 	glutCreateWindow("Shooting Game by Off");
-	glutDisplayFunc(display);
-	glutIdleFunc(display);
+	initialize();
 	glutMainLoop();
 	return 0;
 }
